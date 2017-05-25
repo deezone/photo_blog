58bf87ac5a95be87f9356b6ac6b488948373e3d9
DeeZone <darrendouglas.lee@gmail.com>
2017-05-25 00:04:01 -0400

    STEP: Your first Anglaur application

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: '<h2>Hello World!</h2>',
+    styles: [
+      `
+        h2 {
+            font-family: sans-serif;
+            font-size: 1.2em;
+        }  
+      `
+    ]
+
+})
+export class AppComponent {
+
+}
