12f7dc970205f25c869c16dce0a66896311f3e34
DeeZone <darrendouglas.lee@gmail.com>
2017-05-28 22:59:11 -0400

    Angular Components: Component Templates and Styles

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -6,5 +6,7 @@ import { Component } from "@angular/core";
     styleUrls: ["entry.component.css"]
 })
 export class EntryComponent {
-
+    title: string = "My First Photo";
+    photo: string = "http://placehold.it/800x500?text=Angular Basics";
+    description: string = "A Description of My First Photo";
 }
