85964321fd52f258496cd6917f1090b3d87f34f5
DeeZone <darrendouglas.lee@gmail.com>
2017-05-28 23:53:55 -0400

    Structural Directives

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -9,4 +9,9 @@ export class EntryComponent {
     title: string = "My First Photo";
     photo: string = "http://placehold.it/800x500?text=Angular Basics";
     description: string = "A Description of My First Photo";
+    comments: any[] = [
+        {name: "John", comment: "A comment"},
+        {name: "Jim", comment: "A super comment"},
+        {name: "Jack", comment: "A super cool comment"},
+    ];
 }
