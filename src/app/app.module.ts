534f3dfc4041678362eef57969728637b05c9d1f
DeeZone <darrendouglas.lee@gmail.com>
2017-06-03 20:37:04 -0400

    Configuration to support forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
 import { InMemoryWebApiModule } from "angular-in-memory-web-api";
 import { AppComponent } from "./app.component";
 import { EntryListComponent, EntryComponent, EntryService } from "./entries";
@@ -11,6 +12,7 @@ import { InMemoryEntryService } from "./backend";
     imports: [
         BrowserModule,
         HttpModule,
+        FormsModule,
         InMemoryWebApiModule.forRoot(InMemoryEntryService)
     ],
     providers: [ EntryService ],
