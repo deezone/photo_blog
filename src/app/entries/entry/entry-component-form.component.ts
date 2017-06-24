43ae5b2d8bcf46f7469a11839abcf85118aff046
DeeZone <darrendouglas.lee@gmail.com>
2017-06-23 22:20:58 -0400

    Adds form POSTing functionality

--- src/app/entries/entry/entry-component-form.component.ts
+++ src/app/entries/entry/entry-component-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output, ViewChild } from "@angular/core";
+import { Component, EventEmitter, Input, Output, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { EntryService } from "../shared/entry.service";
 
 @Component({
     selector: "app-entry-component-form",
@@ -9,11 +10,21 @@ import { NgForm } from "@angular/forms";
 export class EntryCommentFormComponent {
     name: string = "";
     comment: string = "";
+    @Input() entryId: number;
     @Output() onCommentAdded = new EventEmitter<{name: string; comment: string}>();
     @ViewChild("commentForm") commentForm: NgForm;
+
+    constructor(private entryService: EntryService) {
+
+    }
+
     onSubmit(commentForm: NgForm) {
         let comment = { name: this.name, comment: this.comment };
-        this.onCommentAdded.emit(comment);
-        this.commentForm.resetForm();
+        this.entryService.addComment(this.entryId, comment)
+            .then(() => {
+                this.onCommentAdded.emit(comment);
+                this.commentForm.resetForm();
+            });
+
     }
 }
